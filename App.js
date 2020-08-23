c321a3f09244f22cd03a5e87ad706235711cc867
Rostislav Emanrus <roostik21@gmail.com>
2020-08-23 21:19:20 +0300

    init

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello World React Native</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
